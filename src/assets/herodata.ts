a03c9797de09b89547ed8f6ded8735ac7c980cac
nde95 <ndennison268@gmail.com>
2024-01-25 15:20:26 -0500

    object for all heros and ids with images

---
+++ src/assets/herodata.ts
@@ -0,0 +1,628 @@
+interface heroDataProps {
+  name: string;
+  image: string;
+  heroId: string;
+}
+
+export const heroData = {
+  Abaddon: {
+    name: "Abaddon",
+    image: "/HeroIcons/abaddon.png",
+    heroId: "abaddon",
+  },
+  Alchemist: {
+    name: "Alchemist",
+    image: "/HeroIcons/alchemist.png",
+    heroId: "alchemist",
+  },
+  AA: {
+    name: "Ancient Apparition",
+    image: "/HeroIcons/ancient_apparition.png",
+    heroId: "ancient_apparition",
+  },
+  AntiMage: {
+    name: "Anti Mage",
+    image: '"/HeroIcons/antimage.png",',
+    heroId: "antimage",
+  },
+  ArcWarden: {
+    name: "Arc Warden",
+    image: "/HeroIcons/arc_warden.png",
+    heroId: "arc_warden",
+  },
+  Axe: {
+    name: "Axe",
+    image: "/HeroIcons/axe.png",
+    heroId: "axe",
+  },
+  Bane: {
+    name: "Bane",
+    image: "/HeroIcons/bane.png",
+    heroId: "bane",
+  },
+  BatRider: {
+    name: "Bat Rider",
+    image: '"/HeroIcons/batrider.png",',
+    heroId: "batrider",
+  },
+  Beastmaster: {
+    name: "Beastmaster",
+    image: "/HeroIcons/beastmaster.png",
+    heroId: "beastmaster",
+  },
+  Bloodseeker: {
+    name: "Bloodseeker",
+    image: "/HeroIcons/bloodseeker.png",
+    heroId: "bloodseeker",
+  },
+  BH: {
+    name: "Bounty Hunter",
+    image: "/HeroIcons/bounty_hunter.png",
+    heroId: "bounty_hunter",
+  },
+  Brewmaster: {
+    name: "Brewmaster",
+    image: "/HeroIcons/brewmaster.png",
+    heroId: "brewmaster",
+  },
+  Bristleback: {
+    name: "Bristleback",
+    image: "/HeroIcons/bristleback.png",
+    heroId: "bristleback",
+  },
+  Broodmother: {
+    name: "Broodmother",
+    image: "/HeroIcons/broodmother.png",
+    heroId: "broodmother",
+  },
+  Centaur: {
+    name: "Centaur Warrunner",
+    image: "/HeroIcons/centaur.png",
+    heroId: "centaur",
+  },
+  CK: {
+    name: "Chaos Knight",
+    image: "/HeroIcons/chaos_knight.png",
+    heroId: "chaos_knight",
+  },
+  Chen: {
+    name: "Chen",
+    image: "/HeroIcons/chen.png",
+    heroId: "chen",
+  },
+  Clinkz: {
+    name: "Clinkz",
+    image: "/HeroIcons/clinkz.png",
+    heroId: "clinkz",
+  },
+  Clockwerk: {
+    name: "Clockwerk",
+    image: "/HeroIcons/rattletrap.png",
+    heroId: "rattletrap",
+  },
+  CM: {
+    name: "Crystal Maiden",
+    image: "/HeroIcons/crystal_maiden.png",
+    heroId: "crystal_maiden",
+  },
+  DarkSeer: {
+    name: "Dark Seer",
+    image: "/HeroIcons/dark_seer.png",
+    heroId: "dark_seer",
+  },
+  DarkWillow: {
+    name: "Dark Willow",
+    image: "/HeroIcons/dark_willow.png",
+    heroId: "dark_willow",
+  },
+  Dawnbreaker: {
+    name: "Dawnbreaker",
+    image: "/HeroIcons/dawnbreaker.png",
+    heroId: "dawnbreaker",
+  },
+  Dazzle: {
+    name: "Dazzle",
+    image: "/HeroIcons/dazzle.png",
+    heroId: "dazzle",
+  },
+  DP: {
+    name: "Death Prophet",
+    image: "/HeroIcons/death_prophet.png",
+    heroId: "death_prophet",
+  },
+  Disruptor: {
+    name: "Disruptor",
+    image: "/HeroIcons/disruptor.png",
+    heroId: "disruptor",
+  },
+  Doom: {
+    name: "Doom",
+    image: "/HeroIcons/doom_bringer.png",
+    heroId: "doom_bringer",
+  },
+  DragonKnight: {
+    name: "Dragon Knight",
+    image: "/HeroIcons/dragon_knight.png",
+    heroId: "dragon_knight",
+  },
+  DrowRanger: {
+    name: "Drow Ranger",
+    image: "/HeroIcons/drow_ranger.png",
+    heroId: "drow_ranger",
+  },
+  ES: {
+    name: "Earth Spirit",
+    image: "/HeroIcons/earth_spirit.png",
+    heroId: "earth_spirit",
+  },
+  EarthShaker: {
+    name: "Earthshaker",
+    image: "/HeroIcons/earthshaker.png",
+    heroId: "earthshaker",
+  },
+  ET: {
+    name: "Elder Titan",
+    image: "/HeroIcons/elder_titan.png",
+    heroId: "elder_titan",
+  },
+  EmberSpirit: {
+    name: "Ember Spirit",
+    image: "/HeroIcons/ember_spirit.png",
+    heroId: "ember_spirit",
+  },
+  Enchantress: {
+    name: "Enchantress",
+    image: "/HeroIcons/enchantress.png",
+    heroId: "enchantress",
+  },
+  Enigma: {
+    name: "Enigma",
+    image: "/HeroIcons/enigma.png",
+    heroId: "enigma",
+  },
+  Void: {
+    name: "Faceless Void",
+    image: "/HeroIcons/faceless_void.png",
+    heroId: "faceless_void",
+  },
+  Grimstroke: {
+    name: "Grimstroke",
+    image: "/HeroIcons/grimstroke.png",
+    heroId: "grimstroke",
+  },
+  Gyrocopter: {
+    name: "Gyrocopter",
+    image: "/HeroIcons/gyrocopter.png",
+    heroId: "gyrocopter",
+  },
+  Hoodwink: {
+    name: "Hoodwink",
+    image: "/HeroIcons/hoodwink.png",
+    heroId: "hoodwink",
+  },
+  Huskar: {
+    name: "Huskar",
+    image: "/HeroIcons/huskar.png",
+    heroId: "huskar",
+  },
+  Invoker: {
+    name: "Invoker",
+    image: "/HeroIcons/invoker.png",
+    heroId: "enigma",
+  },
+  Io: {
+    name: "Io",
+    image: "/HeroIcons/wisp.png",
+    heroId: "wisp",
+  },
+  Jakiro: {
+    name: "Jakiro",
+    image: "/HeroIcons/jakiro.png",
+    heroId: "jakiro",
+  },
+  Juggernaut: {
+    name: "Juggernaut",
+    image: "/HeroIcons/juggernaut.png",
+    heroId: "juggernaut",
+  },
+  KOTL: {
+    name: "Keeper of the light",
+    image: "/HeroIcons/keeper_of_the_light.png",
+    heroId: "keeper_of_the_light",
+  },
+  Kunkka: {
+    name: "Kunkka",
+    image: "/HeroIcons/kunkka.png",
+    heroId: "kunkka",
+  },
+  Legion: {
+    name: "Legion Commander",
+    image: "/HeroIcons/legion_commander.png",
+    heroId: "legion_commander",
+  },
+  Leshrac: {
+    name: "Leshrac",
+    image: "/HeroIcons/leshrac.png",
+    heroId: "leshrac",
+  },
+  Lich: {
+    name: "Lich",
+    image: "/HeroIcons/lich.png",
+    heroId: "lich",
+  },
+  Lifestealer: {
+    name: "Lifestealer",
+    image: "/HeroIcons/life_stealer.png",
+    heroId: "enigma",
+  },
+  Lina: {
+    name: "Lina",
+    image: "/HeroIcons/lina.png",
+    heroId: "lina",
+  },
+  Lion: {
+    name: "Lion",
+    image: "/HeroIcons/lion.png",
+    heroId: "lion",
+  },
+  LoneDruid: {
+    name: "LoneDruid",
+    image: "/HeroIcons/lone_druid.png",
+    heroId: "lone_druid",
+  },
+  Luna: {
+    name: "Luna",
+    image: "/HeroIcons/luna.png",
+    heroId: "luna",
+  },
+  Lycan: {
+    name: "Lycan",
+    image: "/HeroIcons/lycan.png",
+    heroId: "enigma",
+  },
+  Magnus: {
+    name: "Magnus",
+    image: "/HeroIcons/magnataur.png",
+    heroId: "magnataur",
+  },
+  Marci: {
+    name: "Marci",
+    image: "/HeroIcons/marci.png",
+    heroId: "marci",
+  },
+  Mars: {
+    name: "Mars",
+    image: "/HeroIcons/mars.png",
+    heroId: "mars",
+  },
+  Medusa: {
+    name: "Medusa",
+    image: "/HeroIcons/medusa.png",
+    heroId: "medusa",
+  },
+  Meepo: {
+    name: "Meepo",
+    image: "/HeroIcons/meepo.png",
+    heroId: "meepo",
+  },
+  Mirana: {
+    name: "Mirana",
+    image: "/HeroIcons/mirana.png",
+    heroId: "mirana",
+  },
+  Monkey: {
+    name: "Monkey King",
+    image: "/HeroIcons/monkey_king.png",
+    heroId: "monkey_king",
+  },
+  Morph: {
+    name: "Morphling",
+    image: "/HeroIcons/morphling.png",
+    heroId: "morphling",
+  },
+  Muerta: {
+    name: "Muerta",
+    image: "/HeroIcons/muerta.png",
+    heroId: "muerta",
+  },
+  Naga: {
+    name: "Naga Siren",
+    image: "/HeroIcons/naga_siren.png",
+    heroId: "naga_siren",
+  },
+  NP: {
+    name: "Natures Prophet",
+    image: "/HeroIcons/furion.png",
+    heroId: "furion",
+  },
+  Necrophos: {
+    name: "Necrophos",
+    image: "/HeroIcons/necrolyte.png",
+    heroId: "necrolyte",
+  },
+  NightStalker: {
+    name: "Night Stalker",
+    image: "/HeroIcons/night_stalker.png",
+    heroId: "night_stalker",
+  },
+  Nyx: {
+    name: "Nyx Assassin",
+    image: "/HeroIcons/nyx_assassin.png",
+    heroId: "nyx_assassin",
+  },
+  Ogre: {
+    name: "Ogre Magi",
+    image: "/HeroIcons/ogre_magi.png",
+    heroId: "ogre_magi",
+  },
+  Omni: {
+    name: "Omniknight",
+    image: "/HeroIcons/omniknight.png",
+    heroId: "omniknight",
+  },
+  Oracle: {
+    name: "Oracle",
+    image: "/HeroIcons/oracle.png",
+    heroId: "oracle",
+  },
+  Outworld: {
+    name: "Outworld Destroyer",
+    image: "/HeroIcons/obsidian_destroyer.png",
+    heroId: "obsidian_destroyer",
+  },
+  Pango: {
+    name: "Pangolier",
+    image: "/HeroIcons/pangolier.png",
+    heroId: "pangolier",
+  },
+  PA: {
+    name: "Phantom Assassin",
+    image: "/HeroIcons/phantom_assassin.png",
+    heroId: "phantom_assassin",
+  },
+  PL: {
+    name: "Phantom Lancer",
+    image: "/HeroIcons/phantom_lancer.png",
+    heroId: "phantom_lancer",
+  },
+  Phoenix: {
+    name: "Phoenix",
+    image: "/HeroIcons/phoenix.png",
+    heroId: "phoenix",
+  },
+  Primal: {
+    name: "Primal Beast",
+    image: "/HeroIcons/primal_beast.png",
+    heroId: "primal_beast",
+  },
+  Puck: {
+    name: "Puck",
+    image: "/HeroIcons/puck.png",
+    heroId: "puck",
+  },
+  Pudge: {
+    name: "Pudge",
+    image: "/HeroIcons/pudge.png",
+    heroId: "pudge",
+  },
+  Pudgna: {
+    name: "Pugna",
+    image: "/HeroIcons/pugna.png",
+    heroId: "pugna",
+  },
+  QOP: {
+    name: "Queen of Pain",
+    image: "/HeroIcons/queenofpain.png",
+    heroId: "queenofpain",
+  },
+  Razor: {
+    name: "Razor",
+    image: "/HeroIcons/razor.png",
+    heroId: "razor",
+  },
+  Riki: {
+    name: "Riki",
+    image: "/HeroIcons/riki.png",
+    heroId: "riki",
+  },
+  Rubick: {
+    name: "Rubick",
+    image: "/HeroIcons/rubick.png",
+    heroId: "rubick",
+  },
+  SandKing: {
+    name: "Sand King",
+    image: "/HeroIcons/sand_king.png",
+    heroId: "sand_king",
+  },
+  ShadDemon: {
+    name: "Shadow Demon",
+    image: "/HeroIcons/shadow_demon.png",
+    heroId: "shadow_demon",
+  },
+  ShadFiend: {
+    name: "Shadow Fiend",
+    image: "/HeroIcons/nevermore.png",
+    heroId: "nevermore",
+  },
+  ShadShaman: {
+    name: "Shadow Shaman",
+    image: "/HeroIcons/shadow_shaman.png",
+    heroId: "shadow_shaman",
+  },
+  Silencer: {
+    name: "Silencer",
+    image: "/HeroIcons/silencer.png",
+    heroId: "silencer",
+  },
+  Skywrath: {
+    name: "Skywrath Mage",
+    image: "/HeroIcons/skywrath_mage.png",
+    heroId: "skywrath_mage",
+  },
+  Slardar: {
+    name: "Slardar",
+    image: "/HeroIcons/slardar.png",
+    heroId: "slardar",
+  },
+  Slark: {
+    name: "Slark",
+    image: "/HeroIcons/slark.png",
+    heroId: "slark",
+  },
+  Snapfire: {
+    name: "Snapfire",
+    image: "/HeroIcons/snapfire.png",
+    heroId: "snapfire",
+  },
+  Sniper: {
+    name: "Sniper",
+    image: "/HeroIcons/sniper.png",
+    heroId: "sniper",
+  },
+  Spectre: {
+    name: "Spectre",
+    image: "/HeroIcons/spectre.png",
+    heroId: "spectre",
+  },
+  SB: {
+    name: "Spirit Breaker",
+    image: "/HeroIcons/spirit_breaker.png",
+    heroId: "spirit_breaker",
+  },
+  Storm: {
+    name: "Storm Spirit",
+    image: "/HeroIcons/storm_spirit.png",
+    heroId: "storm_spirit",
+  },
+  Sven: {
+    name: "Sven",
+    image: "/HeroIcons/sven.png",
+    heroId: "sven",
+  },
+  Techies: {
+    name: "Techies",
+    image: "/HeroIcons/techies.png",
+    heroId: "techies",
+  },
+  TA: {
+    name: "Templar Assassin",
+    image: "/HeroIcons/templar_assassin.png",
+    heroId: "templar_assassin",
+  },
+  Terrorblade: {
+    name: "Terrorblade",
+    image: "/HeroIcons/terrorblade.png",
+    heroId: "terrorblade",
+  },
+  Tidehunter: {
+    name: "Tidehunter",
+    image: "/HeroIcons/tidehunter.png",
+    heroId: "tidehunter",
+  },
+  Timbersaw: {
+    name: "Timbersaw",
+    image: "/HeroIcons/shredder.png",
+    heroId: "shredder",
+  },
+  Tinker: {
+    name: "Tinker",
+    image: "/HeroIcons/tinker.png",
+    heroId: "tinker",
+  },
+  Tiny: {
+    name: "Tiny",
+    image: "/HeroIcons/tiny.png",
+    heroId: "tiny",
+  },
+  Treant: {
+    name: "Treant Protector",
+    image: "/HeroIcons/treant.png",
+    heroId: "treant",
+  },
+  Troll: {
+    name: "Troll Warlord",
+    image: "/HeroIcons/troll_warlord.png",
+    heroId: "troll_warlord",
+  },
+  Tusk: {
+    name: "Tusk",
+    image: "/HeroIcons/tusk.png",
+    heroId: "tusk",
+  },
+  Underlord: {
+    name: "Underlord",
+    image: "/HeroIcons/abyssal_underlord.png",
+    heroId: "abyssal_underlord",
+  },
+  Undying: {
+    name: "Undying",
+    image: "/HeroIcons/undying.png",
+    heroId: "undying",
+  },
+  Ursa: {
+    name: "Ursa",
+    image: "/HeroIcons/ursa.png",
+    heroId: "ursa",
+  },
+  Venge: {
+    name: "Vengeful Spirit",
+    image: "/HeroIcons/vengefulspirit.png",
+    heroId: "vengefulspirit",
+  },
+  Veno: {
+    name: "Venomancer",
+    image: "/HeroIcons/venomancer.png",
+    heroId: "venomancer",
+  },
+  Viper: {
+    name: "Viper",
+    image: "/HeroIcons/viper.png",
+    heroId: "viper",
+  },
+  Visage: {
+    name: "Visage",
+    image: "/HeroIcons/visage.png",
+    heroId: "visage",
+  },
+  VoidSpirit: {
+    name: "Void Spirit",
+    image: "/HeroIcons/void_spirit.png",
+    heroId: "void_spirit",
+  },
+  Warlock: {
+    name: "Warlock",
+    image: "/HeroIcons/warlock.png",
+    heroId: "warlock",
+  },
+  Weaver: {
+    name: "Weaver",
+    image: "/HeroIcons/weaver.png",
+    heroId: "weaver",
+  },
+  Wind: {
+    name: "Wind Ranger",
+    image: "/HeroIcons/windrunner.png",
+    heroId: "windrunner",
+  },
+  Winter: {
+    name: "Winter Wyvern",
+    image: "/HeroIcons/winter_wyvern.png",
+    heroId: "winter_wyvern",
+  },
+  WitchDoc: {
+    name: "Witch Doctor",
+    image: "/HeroIcons/witch_doctor.png",
+    heroId: "witch_doctor",
+  },
+  Wraith: {
+    name: "Wraith King",
+    image: "/HeroIcons/skeleton_king.png",
+    heroId: "skeleton_king",
+  },
+  Zeus: {
+    name: "Zeus",
+    image: "/HeroIcons/zuus.png",
+    heroId: "zuus",
+  },
+};
