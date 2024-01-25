13cc59dc2c25a946a751d7e6aa9905bf150088db
nde95 <ndennison268@gmail.com>
2024-01-25 17:16:54 -0500

    exporting functionality and taking up full screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <div className="text-xl text-sky-400">Hello World</div>
+      <div className="h-full w-full">
+        <Home />
+      </div>
     </>
   );
 }
