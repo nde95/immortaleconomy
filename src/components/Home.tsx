b50ab10b54de2c665ab4c1eabf34c0cfbe7f917e
nde95 <ndennison268@gmail.com>
2024-01-26 16:09:20 -0500

    fetching from deployment api

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -27,7 +27,7 @@ const Home = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/search", {
+      const response = await fetch("https://joplin.pythonanywhere.com/search", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
