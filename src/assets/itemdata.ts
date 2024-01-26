6387f06328332017fd805b73ae3f0c2c224bd9a2
nde95 <ndennison268@gmail.com>
2024-01-26 12:34:35 -0500

    dummy object for styling based on shape of data

---
+++ src/assets/itemdata.ts
@@ -0,0 +1,58 @@
+export const itemData = [
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUzpRhVSUHZVPDi2sbXUk5LLgpCtbGpLhQv3vrMcC5959Kjl4uHkuS6ar2DwzNS6Z0oi-2U8dSt2QDmqEVsZz31coCdJgE_aVDTrlC4xu--14j84srEY2_T4Q",
+    name: "Genuine Midas Knuckles",
+    sell_listings: 11,
+    sell_price_text: "$21.62",
+  },
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUzpRhVSUHZVPDixc7JSVR4Njtav76kODhr3ebLejFH_5OklZqRnvvnDLbYkmpC185_n-rK-Ynmxg2y-UZkMDj7LdfBd1I2aVnU-AK4weq7g8C6tMnOyHpruXQj43jczkepwUYb7_94yPw",
+    name: "Genuine Razzil's Midas Knuckles",
+    sell_listings: 19,
+    sell_price_text: "$3.72",
+  },
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUzpRhVSUHZVPDi0tvWQVN1KTtFt76sKglj1szKfTxG6c_5kZSOhfn1P4TDl29Y6ctyj9bD-Y3xjUf6_BJlN27wI4aUIwQ5Z1rZrAS3xLq7hZDovZSfn3RhsiYl7XyLzkOxgwYMMLKI_9BaMA",
+    name: "Genuine Eternal Radiance Blades",
+    sell_listings: 101,
+    sell_price_text: "$28.26",
+  },
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUzpRhVSUHZVPDixc7JSVR4Njtav76kODhr3ebLejFH_5OklZqRnvvnDLbYkmpC185_n-rK-Ynmxg2y-UZkMDj7LdfBd1I2aVnU-AK4weq7g8C6tMnOyHpruXQj43jczkepwUYb7_94yPw",
+    name: "Autographed Razzil's Midas Knuckles",
+    sell_listings: 26,
+    sell_price_text: "$3.17",
+  },
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXX4BlROpl9vRpIQljvTuei2fDSX158IAlepa6aJABy1szAfi0Mudjux4Hdla-jZ72AzjgDuJEl2rjH99mh2wC1_0ZkazqmcIfBcwE4NEaQpAYbU2H2pg",
+    name: "Taunt: Ogre Hop",
+    sell_listings: 841,
+    sell_price_text: "$0.08",
+  },
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUzpRhVSUHZVPDixc7JSVR4Njtav76kODhr3ebLejFH_5OklZqRnvvnDLbYkmpC185_n-rK-Ynmxg2y-UZkMDj7LdfBd1I2aVnU-AK4weq7g8C6tMnOyHpruXQj43jczkepwUYb7_94yPw",
+    name: "Inscribed Razzil's Midas Knuckles",
+    sell_listings: 190,
+    sell_price_text: "$2.39",
+  },
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUzpRhVSUHZVPDixc7JSVR4Njtav76kODhr3ebLejFH_5OklZqRnvvnDLbYkmpC185_n-rK-Ynmxg2y-UZkMDj7LdfBd1I2aVnU-AK4weq7g8C6tMnOyHpruXQj43jczkepwUYb7_94yPw",
+    name: "Razzil's Midas Knuckles",
+    sell_listings: 146,
+    sell_price_text: "$2.84",
+  },
+  {
+    icon_url:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUzpRhVSUHZVPDixc7JSVR4Njtav76kODhr3ebLejFH_5OklZqRnvvnDLbYkmpC185_n-rK-Ynmxg2y-UZkMDj7LdfBd1I2aVnU-AK4weq7g8C6tMnOyHpruXQj43jczkepwUYb7_94yPw",
+    name: "Corrupted Razzil's Midas Knuckles",
+    sell_listings: 1,
+    sell_price_text: "$11.21",
+  },
+];
